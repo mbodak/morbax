e695f43f50555374b5e57ea61550e58b7a7e49a3
Marina <bodak.marina.ev@gmail.com>
2017-10-03 19:26:27 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+/**
+ * Created by marina on 03.10.17.
+ */
+
+function openAssesment() {
+    document.getElementById('assesment').style.display = 'block';
+    document.getElementById('assesmentButt').style.borderColor = 'lightgrey';
+    document.getElementById('assesmentButt').style.backgroundColor = 'white';
+    document.getElementById('assesmentButtA').style.color = '#777';
+
+    document.getElementById('profile').style.display = 'none';
+    document.getElementById('profileButt').style.borderColor = '#59068a';
+    document.getElementById('profileButt').style.backgroundColor = '#59068a';
+    document.getElementById('profileButtA').style.color = 'white';
+}
+
+function openProfile() {
+    document.getElementById('profile').style.display = 'block';
+    document.getElementById('profileButt').style.borderColor = 'lightgrey';
+    document.getElementById('profileButt').style.backgroundColor = 'white';
+    document.getElementById('profileButtA').style.color = '#777';
+
+    document.getElementById('assesment').style.display = 'none';
+    document.getElementById('assesmentButt').style.borderColor = '#59068a';
+    document.getElementById('assesmentButt').style.backgroundColor = '#59068a';
+    document.getElementById('assesmentButtA').style.color = 'white';
+}
+
